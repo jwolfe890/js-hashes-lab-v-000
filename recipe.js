05df819649bf022e688b2ce2d846d141786b8a44
jwolfe890 <jwolfe890@gmail.com>
2017-02-13 22:13:17 -0500

    all

--- recipe.js
+++ recipe.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var tacoIngredients = {tortillas: "1 dozen, small"}
+
+function addIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    debugger;
+    return console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
